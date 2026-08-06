8ea9379ca9dba2b962cc28aceb4984ffc77cb4c1
Tzavi727 <tzaviknows@gmail.com>
2026-08-05 22:01:23 -0300

    feat: stable temp cleaner without animation

---
+++ WinTempClear/Program.cs
@@ -0,0 +1,44 @@
+﻿string tempPath = Path.GetTempPath();
+
+var tempFiles = Directory.GetFiles(tempPath);
+
+var tempDirectories = Directory.GetDirectories(tempPath);
+
+int filesCount = 0;
+int failCount = 0;
+
+foreach (var file in tempFiles)
+{
+    try
+    {
+        File.Delete(file);
+        filesCount++;
+    }
+    catch (IOException )
+    {
+        Console.WriteLine($"The file {Path.GetFileName(file)} could not be deleted");
+        failCount++;
+    }
+}
+
+foreach (var dir in tempDirectories)
+{
+    try
+    {
+        Directory.Delete(dir,true);
+        filesCount++;
+    }
+    catch (IOException)
+    {
+        Console.WriteLine($"The directory {Path.GetFileName(dir)} could not be deleted");
+        failCount++;
+    }
+    catch (UnauthorizedAccessException)
+    {
+        Console.WriteLine($"The directory {Path.GetFileName(dir)} could not be deleted");
+        failCount++;
+    }
+}
+
+Console.WriteLine($"{filesCount} Files Deleted!");
+Console.WriteLine($"{failCount} Files couldn't be deleted, Probably by being in use by an application or needing administrator access");
