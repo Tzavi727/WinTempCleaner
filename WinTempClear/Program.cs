f313d843f4dab0d239b4d3d09847f634d849842e
Tzavi727 <tzaviknows@gmail.com>
2026-08-06 11:36:00 -0300

    refactor: remove empty spaces from code

--- WinTempClear/Program.cs
+++ WinTempClear/Program.cs
@@ -4,9 +4,7 @@ FileService fileService = new();
 UIHelper uIHelper = new(fileService);
 
 string tempPath = fileService.GetTempPath();
-
 var tempFiles = fileService.GetTempFiles(tempPath);
-
 var tempDir = fileService.GetTempDirectories(tempPath);
 
 fileService.CleanTempFiles(tempFiles);
